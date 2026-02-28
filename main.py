3460e8be94d89b140de03edf9c4b7fc4d76b3e45
ttan4ick <tyulina.tanechka@yandex.ru>
2026-02-28 17:49:13 +0300

    task 3

--- main.py
+++ main.py
@@ -2,14 +2,45 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('base.html', title='Заготовка')
+    return render_template("base.html", title="Заготовка")
 
-@app.route('/training/<prof>')
+
+@app.route("/training/<prof>")
 def profession(prof):
-    return render_template('prof.html', title='Тренировки', profession=prof)
+    return render_template("prof.html", title="Тренировки", profession=prof)
 
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+
+@app.route("/list_prof/<list>")
+def prof_list(list):
+    professions_list = [
+        "инженер-исследователь",
+        "пилот",
+        "строитель",
+        "экзобиолог",
+        "врач",
+        "инженер по терраформированию",
+        "климатолог",
+        "специалист по радиационной защите",
+        "астрогеолог",
+        "гляциолог",
+        "инженер жизнеобеспечения",
+        "метеоролог",
+        "оператор марсохода",
+        "киберинженер",
+        "штурман",
+        "пилот дронов",
+    ]
+    return render_template(
+        "all_professions.html",
+        title="Профессии",
+        professions=professions_list,
+        list_type=list,
+    )
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
