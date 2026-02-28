30d6587d8ec471cbf36327200eaebd4574e1cd2d
ttan4ick <tyulina.tanechka@yandex.ru>
2026-02-28 15:45:57 +0300

    task 2

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('base.html', title='Заготовка')
 
+@app.route('/training/<prof>')
+def profession(prof):
+    return render_template('prof.html', title='Тренировки', profession=prof)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
